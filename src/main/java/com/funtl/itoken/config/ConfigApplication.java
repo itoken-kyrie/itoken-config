082fca2abb1b2d7ec616d37182e3fb516b3d6234
kyrie <1336861579@qq.com>
2019-08-11 20:48:42 +0800

    init commit

---
+++ src/main/java/com/funtl/itoken/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.funtl.itoken.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author kyrie
+ * @date 2019/8/11 - 19:54
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
